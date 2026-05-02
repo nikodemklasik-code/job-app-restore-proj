648ea63f1db057806ba0316f3e14fcb3971ca802
nikodemklasik-code <nikodem.klasik@gmail.com>
2026-05-03 00:01:35 +0100

    Change 'Tailor Resume' button to 'Tailor CV'

--- frontend/src/components/jobs/JobCardExpanded.tsx
+++ frontend/src/components/jobs/JobCardExpanded.tsx
@@ -371,7 +371,7 @@ export function JobCardExpanded({
                                         ) : (
                                             <>
                                                 <Sparkles className="w-4 h-4 inline mr-2" />
-                                                Tailor Resume
+                                                Tailor CV
                                             </>
                                         )}
                                     </button>
